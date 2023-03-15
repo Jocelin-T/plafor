1a4d33bdf6aa176775b6140abc4a86b1c6082fa8
CavannaLaurent <LaurentReneJoseph.Cavanna@formation.orif.ch>
2023-03-15 13:17:42 +0100

    Create User_type_modelTest.php
    Add User_type_model unit test

---
+++ tests/orif/user/User_type_modelTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Unit test User_modelTest 
+ *
+ * @author      Orif (CaLa)
+ * @link        https://github.com/OrifInformatique
+ * @copyright   Copyright (c), Orif (https://www.orif.ch)
+ */
+namespace User\Models;
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+class User_type_modelTest extends CIUnitTestCase
+{
+    public function testgetUser_type_modelInstance()
+    {
+        $userModel = User_type_model::getInstance();
+        $this->assertTrue($userModel instanceof User_type_model); 
+    }
+
+    public function testgetUser_modelInstance()
+    {
+        $userModel = User_type_model::getInstance();
+        $this->assertFalse($userModel instanceof User_model);
+    }
+}
