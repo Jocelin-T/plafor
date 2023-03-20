4a5e7bed81a3ceefe1a0290bbbda85ded22fa7a7
CavannaLaurent <LaurentReneJoseph.Cavanna@formation.orif.ch>
2023-03-20 14:45:48 +0100

    Update AuthTest.php
    Add unit tests for login and logout methods

--- tests/orif/user/Controllers/AuthTest.php
+++ tests/orif/user/Controllers/AuthTest.php
@@ -11,23 +11,24 @@
 
  use CodeIgniter\Test\CIUnitTestCase;
  use CodeIgniter\Test\ControllerTestTrait;
-
+ 
  class AuthTest extends CIUnitTestCase
 {
     use ControllerTestTrait;
 
     /**
-     * Asserts that the login page is loaded correctly
+     * Asserts that the login page is loaded correctly (no session)
      */
-    public function testlogin()
+    public function testloginPageWithoutSession()
     {
+        // Execute login method of Auth class
         $result = $this->controller(Auth::class)
         ->execute('login');
 
+        // Assertions
         $response = $result->response();
         $this->assertInstanceOf(\CodeIgniter\HTTP\Response::class, $response);
         $this->assertNotEmpty($response->getBody());
-
         $result->assertOK();
         $result->assertHeader('Content-Type', 'text/html; charset=UTF-8');
         $result->assertSeeElement('#username');
@@ -38,5 +39,90 @@
         $result->assertSeeInField('username', '');
         $result->assertSeeInField('password', '');
         $result->assertSeeLink('Se connecter');
+    }
+
+    /**
+     * Asserts that the login page is redirected (no session)
+     */
+    public function testloginPageWithSession()
+    {
+        // Initialize session
+        $_SESSION['logged_in'] = true;
+
+        // Execute login method of Auth class
+        $result = $this->controller(Auth::class)
+        ->execute('login');
+
+        // Assertions
+        $response = $result->response();
+        $this->assertInstanceOf(\CodeIgniter\HTTP\RedirectResponse::class, $response);
+        $this->assertEmpty($response->getBody());
+        $result->assertOK();
+        $result->assertHeader('Content-Type', 'text/html; charset=UTF-8');
+    }
+
+    /**
+     * Asserts that the change_password page is redirected (no session)
+     */
+    public function testchange_passwordPageWithoutSession()
+    {
+        // Execute change_password method of Auth class
+        $result = $this->controller(Auth::class)
+        ->execute('change_password');
+
+        // Assertions
+        $response = $result->response();
+        $this->assertInstanceOf(\CodeIgniter\HTTP\RedirectResponse::class, $response);
+        $this->assertEmpty($response->getBody());
+        $result->assertOK();
+        $result->assertHeader('Content-Type', 'text/html; charset=UTF-8');
+    }
+
+    /**
+     * Asserts that the change_password page is loaded correctly (with session)
+     */
+    public function testchange_passwordPageWithSession()
+    {
+        // Initialize session
+        $_SESSION['logged_in'] = true;
+        $_SESSION['user_access'] = config('\User\Config\UserConfig')->access_lvl_guest;
+
+        // Execute change_password method of Auth class
+        $result = $this->controller(Auth::class)
+        ->execute('change_password');
+
+        // Assertions
+        $response = $result->response();
+        $this->assertInstanceOf(\CodeIgniter\HTTP\Response::class, $response);
+        $this->assertNotEmpty($response->getBody());
+        $result->assertOK();
+        $result->assertHeader('Content-Type', 'text/html; charset=UTF-8');
+        $result->assertSeeElement('#old_password');
+        $result->assertSeeElement('#new_password');
+        $result->assertSeeElement('#confirm_password');
+        $result->assertSeeElement('#btn_change_password');
+        $result->assertDontSeeElement('#fake_element');
+        $result->assertSeeInField('old_password', '');
+        $result->assertSeeInField('new_password', '');
+        $result->assertSeeLink('Annuler');
+    }
+
+    /**
+     * Asserts that logout resets the session
+     */
+    public function testlogout()
+    {
+        // Initialize session
+        $_SESSION['logged_in'] = true;
+
+        // Assertion
+        $this->assertNotEmpty($_SESSION);
+
+        // Execute logout method of Auth class
+        $result = $this->controller(Auth::class)
+        ->execute('logout');
+
+        // Assertion
+        $this->assertEmpty($_SESSION);
     }
 }
